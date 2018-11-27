a56b8566e7e7d8b2587aa7b1f9df3f425a49fbe4
Malex <Malex@malexprojects.com>
2018-11-27 13:10:07 +0100

    Fix #13 in a better way

--- httphelp/constants.py
+++ httphelp/constants.py
@@ -1,6 +1,8 @@
-import os.path
+import os
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+
+_isNotDumb = os.getenv("TERM", "dumb").lower() == "dumb"
 
 # Scroll actions
 SCROLL_LINE_UP = "line up"
@@ -11,8 +13,8 @@ SCROLL_TO_TOP = "to top"
 SCROLL_TO_END = "to end"
 
 # ASCII color codes
-YELLOW = '\033[33m'
-RED = "\033[31m"
-BOLD = '\033[1m'
-UNDERLINE = '\033[4m'
-END = "\033[0m"
+YELLOW = '\033[33m' if _isNotDumb else ''
+RED = "\033[31m" if _isNotDumb else ''
+BOLD = '\033[1m' if _isNotDumb else ''
+UNDERLINE = '\033[4m' if _isNotDumb else ''
+END = "\033[0m" if _isNotDumb else ''
